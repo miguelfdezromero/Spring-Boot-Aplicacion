eaa90dd3fe3d62aa17f007af57e3bc92f2e00be4
miguel <miguelfernandezromero@gmail.com>
2021-04-16 18:15:17 -0500

    Comit inicial con STS. Close #1

---
+++ ProyectoSpringBoot/src/main/java/com/example/demo/ProyectoSpringBootApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoSpringBootApplication.class, args);
+
+	}
+
+}
---
+++ ProyectoSpringBoot/src/test/java/com/example/demo/ProyectoSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
